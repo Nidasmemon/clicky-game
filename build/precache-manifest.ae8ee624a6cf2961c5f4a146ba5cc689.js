3a7861ae610895aab769fff8b881d8143d9fe550
Nida Memon <nidasmemon@gmail.com>
2020-04-30 00:03:22 -0700

    deployed

---
+++ build/precache-manifest.ae8ee624a6cf2961c5f4a146ba5cc689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f659f7a3745bb301c8b31148ff408dd",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "27f79964c8f1d93d7faa",
+    "url": "/clicky-game/static/css/main.17a3787d.chunk.css"
+  },
+  {
+    "revision": "de5c5434ed647499ca7e",
+    "url": "/clicky-game/static/js/2.1cc54414.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/clicky-game/static/js/2.1cc54414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f79964c8f1d93d7faa",
+    "url": "/clicky-game/static/js/main.bc44e1ab.chunk.js"
+  },
+  {
+    "revision": "823b3864c12148873107",
+    "url": "/clicky-game/static/js/runtime-main.59b16ebf.js"
+  }
+]);
